2c6089136b426bf5d6d38011e861e7cf5f80f05b
kim sunchul <ksc89kim@kim-ui-iMac.local>
2018-01-01 21:37:56 +0900

    페이지뷰 추가 및 오토레이 아웃 코드 추가

---
+++ CouponApp/CouponApp/cell/PublicMerchantTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PublicMerchantTableViewCell.swift
+//  CouponApp
+//
+//  Created by kim sunchul on 2017. 12. 31..
+//  Copyright © 2017년 kim sunchul. All rights reserved.
+//
+
+import UIKit
+
+class PublicMerchantTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- CouponApp/CouponApp/viewController/FindMerchantViewController.swift
+++ CouponApp/CouponApp/viewController/FindMerchantViewController.swift
@@ -8,12 +8,41 @@
 
 import UIKit
 
-class FindMerchantViewController: UIViewController {
+class FindMerchantViewController: UIViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+
+    var pageController:UIPageViewController?
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        pageController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
+        
+        let publicFindMerchantTableView = viewControllerAtIndex(index: 0)!
+        let viewControllers:[UIViewController] = [publicFindMerchantTableView]
+        let mainView = self.view.viewWithTag(1)
+        let topView = mainView?.viewWithTag(1)
+        let pageView = self.pageController!.view
+        pageView?.translatesAutoresizingMaskIntoConstraints = false
+        pageController!.setViewControllers(viewControllers, direction: .forward, animated: false, completion: nil)
+        self.addChildViewController(pageController!)
+        mainView?.addSubview(pageView!)
+        
+        let pageViewWidthLayout = NSLayoutConstraint(item: pageView!, attribute: .width, relatedBy: .equal,
+                                                     toItem: mainView, attribute: .width, multiplier: 1, constant: 0)
+        let pageViewCenterLayout = NSLayoutConstraint(item: pageView!, attribute: .centerX, relatedBy: .equal,
+                                                      toItem: mainView, attribute: .centerX, multiplier: 1, constant: 0)
+        let pageViewTopLayout = NSLayoutConstraint(item: pageView!, attribute: .top, relatedBy: .equal,
+                                                   toItem: topView, attribute: .top, multiplier: 1, constant: 30)
+        let pageViewBottomLayout = NSLayoutConstraint(item: pageView!, attribute: .bottom, relatedBy: .equal,
+                                                      toItem: mainView, attribute: .bottom, multiplier: 1, constant: 0)
+        mainView?.addConstraints([pageViewWidthLayout,pageViewCenterLayout,pageViewTopLayout,pageViewBottomLayout])
+        
+        pageController?.delegate = self
+        pageController?.dataSource = self
+        pageController?.view.backgroundColor = UIColor.black
+        pageController!.didMove(toParentViewController: self)
         // Do any additional setup after loading the view.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,7 +50,22 @@ class FindMerchantViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    func viewControllerAtIndex(index:Int) -> UIViewController? {
+        let storyBoard = UIStoryboard(name:"FindMerchantPageView", bundle:Bundle.main)
+        let dataViewControler = storyBoard.instantiateViewController(withIdentifier: "publicMerchantTableView")
+        return dataViewControler
+    }
+    
+    
+    //MARK - PageViewController
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        return viewControllerAtIndex(index: 0)
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        return viewControllerAtIndex(index: 0)
+    }
+    
     /*
     // MARK: - Navigation
 
