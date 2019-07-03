e6a9c6a9695bceeaa5dad1309ad1794db41e8ce3
kim sunchul <ksc89kim@naver.com>
2019-07-03 22:05:14 +0900

    -  스탬프 변경

--- CouponApp/CouponApp/view/IntroStampView.swift
+++ CouponApp/CouponApp/view/IntroStampView.swift
@@ -20,44 +20,47 @@ class IntroStampView: UIView, CAAnimationDelegate{
     }
     
     override func draw(_ rect: CGRect) {
-        let rateX:CGFloat = 0.4
-        let rateY:CGFloat = 0.4
         
+        let originalWidth:CGFloat = 160.08163265306
+        let originalHeight:CGFloat = 169.222222222223
         
-        let adjHorzPos:CGFloat = 0 * rateX; //391
-        let adjVertPos:CGFloat = 445 * rateY; //445
+        let rateX:CGFloat = rect.size.width/originalWidth;
+        let rateY:CGFloat = rect.size.height/originalHeight;
+        let adjHorzPos:CGFloat = 0
+        let adjVertPos:CGFloat = 0
         
         let drawing = UIBezierPath()
-        drawing.move(to: CGPoint(x: (252 * rateX) + adjHorzPos, y:( -268 * rateY) + adjVertPos))
-        drawing.addCurve(to: CGPoint(x: (234 * rateX) + adjHorzPos, y: (-213 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (252 * rateX) + adjHorzPos, y: (-268 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (233 * rateX) + adjHorzPos, y: (-240 * rateY) + adjVertPos))
-        drawing.addCurve(to: CGPoint(x: (252 * rateX) + adjHorzPos, y: (-145 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (233 * rateX) + adjHorzPos, y: (-194 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (236 * rateX) + adjHorzPos, y: (-163 * rateY) + adjVertPos))
-        drawing.addCurve(to: CGPoint(x: (287 * rateX) + adjHorzPos, y: (-124 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (268 * rateX) + adjHorzPos, y: (-128 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (287 * rateX) + adjHorzPos, y: (-124 * rateY) + adjVertPos))
-        drawing.addCurve(to: CGPoint(x: (370 * rateX) + adjHorzPos, y: (-124 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (287 * rateX) + adjHorzPos, y: (-124 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (370 * rateX) + adjHorzPos, y: (-124 * rateY) + adjVertPos))
-        drawing.addCurve(to: CGPoint(x: (391 * rateX) + adjHorzPos, y: (-97 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (370 * rateX) + adjHorzPos, y: (-124 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (389 * rateX) + adjHorzPos, y: (-122 * rateY) + adjVertPos))
-        drawing.addCurve(to: CGPoint(x: (391 * rateX) + adjHorzPos, y: (-61 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (393 * rateX) + adjHorzPos, y: (-73 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (391 * rateX) + adjHorzPos, y: (-61 * rateY) + adjVertPos))
-        drawing.addCurve(to: CGPoint(x: (351 * rateX) + adjHorzPos, y: (-42 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (391 * rateX) + adjHorzPos, y: (-61 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (386 * rateX) + adjHorzPos, y: (-41 * rateY) + adjVertPos))
-        drawing.addCurve(to: CGPoint(x: (272 * rateX) + adjHorzPos, y: (-42 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (301 * rateX) + adjHorzPos, y: (-42 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (272 * rateX) + adjHorzPos, y: (-42 * rateY) + adjVertPos))
-        drawing.addCurve(to: CGPoint(x: (26 * rateX) + adjHorzPos, y: (-42 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (272 * rateX) + adjHorzPos, y: (-42 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (26 * rateX) + adjHorzPos, y: (-42 * rateY) + adjVertPos))
-        drawing.addCurve(to: CGPoint(x: (2 * rateX) + adjHorzPos, y: (-65 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (26 * rateX) + adjHorzPos, y: (-42 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (7 * rateX) + adjHorzPos, y: (-41 * rateY) + adjVertPos))
-        drawing.addCurve(to: CGPoint(x: (27 * rateX) + adjHorzPos, y: (-124 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (-2 * rateX) + adjHorzPos, y: (-90 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (1 * rateX) + adjHorzPos, y: (-120 * rateY) + adjVertPos))
-        drawing.addCurve(to: CGPoint(x: (94 * rateX) + adjHorzPos, y: (-125 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (53 * rateX) + adjHorzPos, y: (-129 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (94 * rateX) + adjHorzPos, y: (-125 * rateY) + adjVertPos))
-        drawing.addCurve(to: CGPoint(x: (144 * rateX) + adjHorzPos, y: (-155 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (94 * rateX) + adjHorzPos, y: (-125 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (127 * rateX) + adjHorzPos, y: (-128 * rateY) + adjVertPos))
-        drawing.addCurve(to: CGPoint(x: (157 * rateX) + adjHorzPos, y: (-213 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (160 * rateX) + adjHorzPos, y: (-181 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (157 * rateX) + adjHorzPos, y: (-213 * rateY) + adjVertPos))
-        drawing.addCurve(to: CGPoint(x: (140 * rateX) + adjHorzPos, y: (-264 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (157 * rateX) + adjHorzPos, y: (-213 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (154 * rateX) + adjHorzPos, y: (-243 * rateY) + adjVertPos))
-        drawing.addCurve(to: CGPoint(x: (114 * rateX) + adjHorzPos, y: (-333 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (126 * rateX) + adjHorzPos, y: (-284 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (115 * rateX) + adjHorzPos, y: (-320 * rateY) + adjVertPos))
-        drawing.addCurve(to: CGPoint(x: (139 * rateX) + adjHorzPos, y: (-417 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (113 * rateX) + adjHorzPos, y: (-346 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (121 * rateX) + adjHorzPos, y: (-397 * rateY) + adjVertPos))
-        drawing.addCurve(to: CGPoint(x: (196 * rateX) + adjHorzPos, y: (-445 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (161 * rateX) + adjHorzPos, y: (-439 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (167 * rateX) + adjHorzPos, y: (-442 * rateY) + adjVertPos))
-        drawing.addCurve(to: CGPoint(x: (265 * rateX) + adjHorzPos, y: (-399 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (225 * rateX) + adjHorzPos, y: (-449 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (254 * rateX) + adjHorzPos, y: (-419 * rateY) + adjVertPos))
-        drawing.addCurve(to: CGPoint(x: (278 * rateX) + adjHorzPos, y: (-321 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (276 * rateX) + adjHorzPos, y: (-379 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (283 * rateX) + adjHorzPos, y: (-353 * rateY) + adjVertPos))
-        drawing.close()
+        drawing.move(to: CGPoint(x: (53 * rateX) + adjHorzPos, y:( 18 * rateY) + adjVertPos))
+        drawing.addCurve(to: CGPoint(x: (70 * rateX) + adjHorzPos, y: (3 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (53 * rateX) + adjHorzPos, y: (18 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (59 * rateX) + adjHorzPos, y: (7 * rateY) + adjVertPos))
+        drawing.addCurve(to: CGPoint(x: (94 * rateX) + adjHorzPos, y: (3 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (70 * rateX) + adjHorzPos, y: (3 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (78 * rateX) + adjHorzPos, y: (-2 * rateY) + adjVertPos))
+        drawing.addCurve(to: CGPoint(x: (115 * rateX) + adjHorzPos, y: (37 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (94 * rateX) + adjHorzPos, y: (3 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (114 * rateX) + adjHorzPos, y: (11 * rateY) + adjVertPos))
+        drawing.addCurve(to: CGPoint(x: (103 * rateX) + adjHorzPos, y: (66 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (115 * rateX) + adjHorzPos, y: (37 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (117 * rateX) + adjHorzPos, y: (51 * rateY) + adjVertPos))
+        drawing.addCurve(to: CGPoint(x: (97 * rateX) + adjHorzPos, y: (92 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (103 * rateX) + adjHorzPos, y: (66 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (96 * rateX) + adjHorzPos, y: (76 * rateY) + adjVertPos))
+        drawing.addCurve(to: CGPoint(x: (106 * rateX) + adjHorzPos, y: (109 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (97 * rateX) + adjHorzPos, y: (92 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (96 * rateX) + adjHorzPos, y: (102 * rateY) + adjVertPos))
+        drawing.addCurve(to: CGPoint(x: (118 * rateX) + adjHorzPos, y: (115 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (106 * rateX) + adjHorzPos, y: (109 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (113 * rateX) + adjHorzPos, y: (115 * rateY) + adjVertPos))
+        drawing.addCurve(to: CGPoint(x: (152 * rateX) + adjHorzPos, y: (116 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (118 * rateX) + adjHorzPos, y: (115 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (152 * rateX) + adjHorzPos, y: (116 * rateY) + adjVertPos))
+        drawing.addCurve(to: CGPoint(x: (161 * rateX) + adjHorzPos, y: (124 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (152 * rateX) + adjHorzPos, y: (116 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (158 * rateX) + adjHorzPos, y: (116 * rateY) + adjVertPos))
+        drawing.addCurve(to: CGPoint(x: (161 * rateX) + adjHorzPos, y: (135 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (161 * rateX) + adjHorzPos, y: (124 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (161 * rateX) + adjHorzPos, y: (135 * rateY) + adjVertPos))
+        drawing.addCurve(to: CGPoint(x: (151 * rateX) + adjHorzPos, y: (146 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (161 * rateX) + adjHorzPos, y: (135 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (162 * rateX) + adjHorzPos, y: (143 * rateY) + adjVertPos))
+        drawing.addCurve(to: CGPoint(x: (10 * rateX) + adjHorzPos, y: (145 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (151 * rateX) + adjHorzPos, y: (146 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (10 * rateX) + adjHorzPos, y: (145 * rateY) + adjVertPos))
+        drawing.addCurve(to: CGPoint(x: (2 * rateX) + adjHorzPos, y: (138 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (10 * rateX) + adjHorzPos, y: (145 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (5 * rateX) + adjHorzPos, y: (144 * rateY) + adjVertPos))
+        drawing.addCurve(to: CGPoint(x: (1 * rateX) + adjHorzPos, y: (125 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (2 * rateX) + adjHorzPos, y: (138 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (1 * rateX) + adjHorzPos, y: (125 * rateY) + adjVertPos))
+        drawing.addCurve(to: CGPoint(x: (11 * rateX) + adjHorzPos, y: (116 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (1 * rateX) + adjHorzPos, y: (125 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (3 * rateX) + adjHorzPos, y: (117 * rateY) + adjVertPos))
+        drawing.addCurve(to: CGPoint(x: (39 * rateX) + adjHorzPos, y: (116 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (11 * rateX) + adjHorzPos, y: (116 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (39 * rateX) + adjHorzPos, y: (116 * rateY) + adjVertPos))
+        drawing.addCurve(to: CGPoint(x: (60 * rateX) + adjHorzPos, y: (103 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (39 * rateX) + adjHorzPos, y: (116 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (55 * rateX) + adjHorzPos, y: (111 * rateY) + adjVertPos))
+        drawing.addCurve(to: CGPoint(x: (63 * rateX) + adjHorzPos, y: (80 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (60 * rateX) + adjHorzPos, y: (103 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (67 * rateX) + adjHorzPos, y: (94 * rateY) + adjVertPos))
+        drawing.addCurve(to: CGPoint(x: (50 * rateX) + adjHorzPos, y: (54 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (63 * rateX) + adjHorzPos, y: (80 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (57 * rateX) + adjHorzPos, y: (63 * rateY) + adjVertPos))
+        drawing.addCurve(to: CGPoint(x: (47 * rateX) + adjHorzPos, y: (32 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (50 * rateX) + adjHorzPos, y: (54 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (45 * rateX) + adjHorzPos, y: (42 * rateY) + adjVertPos))
+        drawing.addCurve(to: CGPoint(x: (53 * rateX) + adjHorzPos, y: (18 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (48 * rateX) + adjHorzPos, y: (24 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (53 * rateX) + adjHorzPos, y: (18 * rateY) + adjVertPos))
 
-        drawing.move(to: CGPoint(x: (379 * rateX) + adjHorzPos, y:( -32 * rateY) + adjVertPos))
-        drawing.addCurve(to: CGPoint(x: (12 * rateX) + adjHorzPos, y: (-32 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (379 * rateX) + adjHorzPos, y: (-32 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (12 * rateX) + adjHorzPos, y: (-32 * rateY) + adjVertPos))
-        drawing.addCurve(to: CGPoint(x: (1 * rateX) + adjHorzPos, y: (-14 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (12 * rateX) + adjHorzPos, y: (-32 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (1 * rateX) + adjHorzPos, y: (-30 * rateY) + adjVertPos))
-        drawing.addCurve(to: CGPoint(x: (22 * rateX) + adjHorzPos, y: (-1 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (0 * rateX) + adjHorzPos, y: (1 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (22 * rateX) + adjHorzPos, y: (-1 * rateY) + adjVertPos))
-        drawing.addCurve(to: CGPoint(x: (379 * rateX) + adjHorzPos, y: (-1 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (22 * rateX) + adjHorzPos, y: (-1 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (379 * rateX) + adjHorzPos, y: (-1 * rateY) + adjVertPos))
-        drawing.addCurve(to: CGPoint(x: (392 * rateX) + adjHorzPos, y: (-16 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (379 * rateX) + adjHorzPos, y: (-1 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (391 * rateX) + adjHorzPos, y: (-3 * rateY) + adjVertPos))
-        
+        drawing.move(to: CGPoint(x: (88 * rateX) + adjHorzPos, y:( 149 * rateY) + adjVertPos))
+        drawing.addCurve(to: CGPoint(x: (156 * rateX) + adjHorzPos, y: (149 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (88 * rateX) + adjHorzPos, y: (149 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (156 * rateX) + adjHorzPos, y: (149 * rateY) + adjVertPos))
+        drawing.addCurve(to: CGPoint(x: (161 * rateX) + adjHorzPos, y: (154 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (156 * rateX) + adjHorzPos, y: (149 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (161 * rateX) + adjHorzPos, y: (150 * rateY) + adjVertPos))
+        drawing.addCurve(to: CGPoint(x: (157 * rateX) + adjHorzPos, y: (160 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (161 * rateX) + adjHorzPos, y: (154 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (162 * rateX) + adjHorzPos, y: (158 * rateY) + adjVertPos))
+        drawing.addCurve(to: CGPoint(x: (6 * rateX) + adjHorzPos, y: (160 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (157 * rateX) + adjHorzPos, y: (160 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (6 * rateX) + adjHorzPos, y: (160 * rateY) + adjVertPos))
+        drawing.addCurve(to: CGPoint(x: (1 * rateX) + adjHorzPos, y: (154 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (6 * rateX) + adjHorzPos, y: (160 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (1 * rateX) + adjHorzPos, y: (160 * rateY) + adjVertPos))
+        drawing.addCurve(to: CGPoint(x: (6 * rateX) + adjHorzPos, y: (149 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (1 * rateX) + adjHorzPos, y: (154 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (1 * rateX) + adjHorzPos, y: (149 * rateY) + adjVertPos))
+        drawing.addCurve(to: CGPoint(x: (88 * rateX) + adjHorzPos, y: (149 * rateY) + adjVertPos), controlPoint1: CGPoint(x: (88 * rateX) + adjHorzPos, y: (149 * rateY) + adjVertPos), controlPoint2: CGPoint(x: (88 * rateX) + adjHorzPos, y: (149 * rateY) + adjVertPos))
         drawing.close()
         
         drawLayer.path = drawing.cgPath
