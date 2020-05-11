3fd58b5bc783a86c2f7ac00ebf5fd6ad70c3ea1a
kim sunchul <ksc89kim@naver.com>
2020-05-11 18:08:47 +0900

    - CouponListViewController 리팩토링중

--- CouponApp/CouponApp/viewController/CouponListViewController.swift
+++ CouponApp/CouponApp/viewController/CouponListViewController.swift
@@ -9,17 +9,17 @@
 import UIKit
 
 /*
-     쿠폰 리스트 뷰 컨트롤러
-     - 현재 가지고 있는 쿠폰 갯수와, 채울 수 있는 최대 쿠폰 갯수를 보여주는 뷰 컨트롤러
-     - 쿠폰 소진하기 및 쿠폰 요청하기 기능이 있음.
+ 쿠폰 리스트 뷰 컨트롤러
+ - 현재 가지고 있는 쿠폰 갯수와, 채울 수 있는 최대 쿠폰 갯수를 보여주는 뷰 컨트롤러
+ - 쿠폰 소진하기 및 쿠폰 요청하기 기능이 있음.
  */
 class CouponListViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     @IBOutlet weak var myCollectionView: UICollectionView!
     @IBOutlet weak var backgroundRoundedView: UIView!
-    @IBOutlet weak var bottomRoundedView: UIView!
+    @IBOutlet weak var bottomButtonRoundedView: UIView!
     @IBOutlet weak var dotLineView: UIView!
-    @IBOutlet weak var leftRoundedView: UIView!
-    @IBOutlet weak var rightRoundedView: UIView!
+    @IBOutlet weak var leftHoleView: UIView!
+    @IBOutlet weak var rightHoleView: UIView!
     
     private let cellSize:CGSize = CGSize(width: 50 , height:50)
     private var selectCouponIndex:NSInteger?
@@ -27,7 +27,7 @@ class CouponListViewController: UIViewController, UICollectionViewDataSource, UI
     
     var userMerchantData:UserCoupon?
     var merchantData:MerchantImpl?
-  
+    
     deinit {
         dotLineView.layer.removeObserver(self, forKeyPath:"bounds")
     }
@@ -36,7 +36,7 @@ class CouponListViewController: UIViewController, UICollectionViewDataSource, UI
         super.viewDidLoad()
         setUI()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -45,34 +45,52 @@ class CouponListViewController: UIViewController, UICollectionViewDataSource, UI
     func setUI() {
         self.navigationItem.title = merchantData?.name
         
+        setBackgroundRoundedView(view:backgroundRoundedView)
+        setHoleView(view: leftHoleView)
+        setHoleView(view: rightHoleView)
+        setBottomButtonRoundedView()
+        setCollectionView()
+        addDashLineAndObserver()
+    }
+    
+    func setBottomButtonRoundedView() {
+        bottomButtonRoundedView.layer.borderWidth = 1
+        bottomButtonRoundedView.layer.borderColor = UIColor.couponGrayColor2.cgColor
+    }
+    
+    func setBackgroundRoundedView(view:UIView) {
+        view.layer.borderWidth = 1
+        view.layer.borderColor = UIColor.couponGrayColor1.cgColor
+        view.layer.cornerRadius = 10
+    }
+    
+    func setHoleView(view:UIView) {
+        view.layer.borderWidth = 1
+        view.layer.borderColor = UIColor.couponGrayColor1.cgColor
+        view.layer.cornerRadius = view.frame.size.width/2
+    }
+    
+    func setCollectionView() {
+        myCollectionView.setCollectionViewLayout(getCollectionViewFlowLayout(), animated: true)
+        myCollectionView.reloadData()
+    }
+    
+    func getCollectionViewFlowLayout() -> UICollectionViewFlowLayout {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .vertical
         layout.itemSize = cellSize
         layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
         layout.minimumLineSpacing = 5.0
         layout.minimumInteritemSpacing = 5.0
-        myCollectionView.setCollectionViewLayout(layout, animated: true)
-        myCollectionView.reloadData()
-        
-        bottomRoundedView.layer.borderWidth = 1
-        bottomRoundedView.layer.borderColor = UIColor.couponGrayColor2.cgColor
-        
+        return layout
+    }
+    
+    func addDashLineAndObserver() {
         dotLineView.addDashLine(dashLayer: dashLineLayer, color: UIColor.couponGrayColor1, lineWidth: 2)
         dotLineView.layer.addObserver(self, forKeyPath: "bounds", options: .new, context:nil)
-        
-        let roundedColor = UIColor.couponGrayColor1
-        self.setRoundedView(view:backgroundRoundedView, width: 1, color:roundedColor , radius: 10)
-        self.setRoundedView(view: leftRoundedView, width: 1, color: roundedColor, radius:leftRoundedView.frame.size.width/2)
-        self.setRoundedView(view: rightRoundedView, width: 1, color: roundedColor, radius:leftRoundedView.frame.size.width/2)
     }
     
-    func setRoundedView(view:UIView, width:CGFloat, color:UIColor, radius:CGFloat) {
-        view.layer.borderWidth = width
-        view.layer.borderColor = color.cgColor
-        view.layer.cornerRadius = radius
-    }
-    
-    // 쿠폰 요청하기
+    // MARK: - 쿠폰 요청하기
     @IBAction func onRequestCoupon(_ sender: Any) {
         guard let couponCount = userMerchantData?.couponCount, let merchant =  merchantData else {
             print("onRequestCoupon - userMerchantData, merchantData error")
@@ -102,7 +120,7 @@ class CouponListViewController: UIViewController, UICollectionViewDataSource, UI
         })
     }
     
-    // 쿠폰 사용하기
+    // MARK: - 쿠폰 사용하기
     @IBAction func onUseCopon(_ sender: Any) {
         guard let couponCount = userMerchantData?.couponCount, let merchant = merchantData else {
             print("onUseCopon - userMerchantData, merchantData error")
@@ -119,7 +137,7 @@ class CouponListViewController: UIViewController, UICollectionViewDataSource, UI
             print("onUseCopon - userId error")
             return
         }
-
+        
         CouponData.updateUesrCoupon(userId: userId, merchantId: merchant.merchantId, couponCount: 0, complete: { [weak self] isSuccessed in
             if isSuccessed {
                 self?.showCustomPopup(title: "successUseCouponTitle".localized, message: "successUseCouponContent".localized,callback: nil)
@@ -130,7 +148,7 @@ class CouponListViewController: UIViewController, UICollectionViewDataSource, UI
             }
         })
     }
- 
+    
     
     // MARK: -  UICollectionViewDataSource
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
