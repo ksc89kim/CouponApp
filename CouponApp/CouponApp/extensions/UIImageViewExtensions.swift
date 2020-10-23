89aa88d43f5808c7f857cfd006d48c2cda245097
kim sunchul <ksc89kim@naver.com>
2020-10-23 23:16:11 +0900

    라이브러리 업데이트 및 RxSwift 추가

--- CouponApp/CouponApp/extensions/UIImageViewExtensions.swift
+++ CouponApp/CouponApp/extensions/UIImageViewExtensions.swift
@@ -11,31 +11,32 @@ import Alamofire
 import AlamofireImage
 
 extension UIImageView {
-    func downloadedFrom(link: String, contentMode mode: UIViewContentMode = .scaleAspectFit) {
-        contentMode = mode
-        Alamofire.request(link).responseImage(completionHandler: { [weak self] response in
-            guard let image = response.result.value else {
-                return
-            }
-            self?.image = image
-        })
+
+  func downloadedFrom(link: String, contentMode mode: UIViewContentMode = .scaleAspectFit) {
+    contentMode = mode
+    AF.request(URL(fileURLWithPath: link)).responseImage { [weak self] response in
+      guard let image = response.value else {
+        return
+      }
+      self?.image = image
     }
-    
-    func setCropRoundedImage(image: UIImage) {
-        let frame = CGRect(x: 0, y: 0, width: self.frame.size.width, height: self.frame.size.height)
-        let imageView: UIImageView = UIImageView(frame: frame)
-        imageView.backgroundColor = UIColor.clear
-        imageView.contentMode = .scaleAspectFill
-        imageView.image = image
-        imageView.layer.masksToBounds = true
-        imageView.layer.cornerRadius = frame.size.width/2
-        
-        UIGraphicsBeginImageContext(frame.size)
-        imageView.layer.render(in: UIGraphicsGetCurrentContext()!)
-        let roundedImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        self.image = roundedImage
-    }
+  }
+
+  func setCropRoundedImage(image: UIImage) {
+    let frame = CGRect(x: 0, y: 0, width: self.frame.size.width, height: self.frame.size.height)
+    let imageView: UIImageView = UIImageView(frame: frame)
+    imageView.backgroundColor = UIColor.clear
+    imageView.contentMode = .scaleAspectFill
+    imageView.image = image
+    imageView.layer.masksToBounds = true
+    imageView.layer.cornerRadius = frame.size.width/2
+
+    UIGraphicsBeginImageContext(frame.size)
+    imageView.layer.render(in: UIGraphicsGetCurrentContext()!)
+    let roundedImage = UIGraphicsGetImageFromCurrentImageContext()
+    UIGraphicsEndImageContext()
+    self.image = roundedImage
+  }
 
 }
 
--- CouponApp/CouponApp/network/CouponNetwork.swift
+++ CouponApp/CouponApp/network/CouponNetwork.swift
@@ -11,196 +11,199 @@ import Alamofire
 import SVProgressHUD
 
 /*
-     쿠폰 네트워크 관리자
+ 쿠폰 네트워크 관리자
  */
 final class CouponNetwork : DataController {
-    var mainUrl:String = "http://192.168.0.11:8080/CouponProject/"
-    
-    func signup(phoneNumber:String, password:String, name:String, complete: @escaping DataCallback) {
-        let fullUrl = "\(mainUrl)user_sign"
-        let parameter = ["phone_number":phoneNumber,"password":password,"name":name]
-        CouponNetwork.showProgress()
-        Alamofire.request(fullUrl, method:.post, parameters: parameter)
-            .validate().responseJSON(completionHandler: { (response) -> Void in
-                CouponNetwork.closeProgress()
-                if CouponNetwork.checkResponseData(response) {
-                    complete(true)
-                } else {
-                    complete(false)
-                }
-            })
+
+  var mainUrl:String = "http://192.168.0.11:8080/CouponProject/"
+
+  func signup(phoneNumber:String, password:String, name:String, complete: @escaping DataCallback) {
+    let fullUrl = "\(mainUrl)user_sign"
+    let parameter = ["phone_number":phoneNumber,"password":password,"name":name]
+    CouponNetwork.showProgress()
+    let request = AF.request(URL(fileURLWithPath: fullUrl), method: .get, parameters: parameter)
+
+    request.responseJSON { (response) -> Void in
+      CouponNetwork.closeProgress()
+      if CouponNetwork.checkResponseData(response) {
+        complete(true)
+      } else {
+        complete(false)
+      }
     }
-    
-    func getUserData(phoneNumber:String, complete: @escaping DataCallback) {
-        let fullUrl = "\(mainUrl)user_data"
-        let parameter = ["phone_number":phoneNumber, "mode":"GetUserInfo"]
-        CouponNetwork.showProgress()
-        Alamofire.request(fullUrl, method:.post, parameters: parameter)
-            .validate().responseJSON(completionHandler: { (response) -> Void in
-                CouponNetwork.closeProgress()
-                if CouponNetwork.checkResponseData(response) {
-                    do {
-                        CouponSignleton.instance.userData =  try JSONDecoder().decode(User.self, from: response.data!)
-                        complete(true)
-                    } catch {
-                         complete(false)
-                    }
-                } else {
-                    complete(false)
-                }
-            })
-    }
-    
-    func checkPassword(phoneNumber:String, password:String, complete: @escaping DataCallback) {
-        let fullUrl = "\(mainUrl)user_data"
-        let parameter = ["phone_number":phoneNumber, "password":password, "mode":"CheckUserPassword"]
-        CouponNetwork.showProgress()
-        Alamofire.request(fullUrl, method:.post, parameters: parameter)
-            .validate().responseJSON(completionHandler: { (response) -> Void in
-                CouponNetwork.closeProgress()
-                if CouponNetwork.checkResponseData(response) {
-                    do {
-                        CouponSignleton.instance.userData =  try JSONDecoder().decode(User.self, from: response.data!)
-                        complete(true)
-                    } catch {
-                        complete(false)
-                    }
-                } else {
-                    complete(false)
-                }
-            })
-    }
-    
-    func getMerchantData(complete: @escaping DataCallback) {
-        let fullUrl = "\(mainUrl)merchant_data"
-        let parameter = ["mode":"GetMerchantData"]
-        CouponNetwork.showProgress()
-        Alamofire.request(fullUrl, method:.get, parameters: parameter)
-            .validate().responseJSON(completionHandler: { (response) -> Void in
-                CouponNetwork.closeProgress()
-                if CouponNetwork.checkResponseData(response) {
-                    do {
-                        CouponSignleton.instance.merchantList = try JSONDecoder().decode(MerchantImplList.self, from: response.data!)
-                        complete(true)
-                    } catch {
-                        print("error \(error)")
-                        complete(false)
-                    }
-                } else {
-                    complete(false)
-                }
-            })
-    }
-    
-    func insertUserCoupon(userId:Int, merchantId:Int, complete: @escaping DataCallback) {
-        let fullUrl = "\(mainUrl)coupon_data"
-        let parameter = ["mode":"InsertCouponData","user_id":userId, "merchant_id":merchantId] as [String : Any]
-        CouponNetwork.showProgress()
-        Alamofire.request(fullUrl, method:.post, parameters: parameter)
-            .validate().responseJSON(completionHandler: { (response) -> Void in
-                CouponNetwork.closeProgress()
-                if CouponNetwork.checkResponseData(response) {
-                    complete(true)
-                } else {
-                    complete(false)
-                }
-            })
-    }
-    
-    func checkUserCoupon(userId:Int, merchantId:Int, complete: @escaping DataCallback) {
-        let fullUrl = "\(mainUrl)coupon_data"
-        let parameter = ["mode":"CheckCouponData","user_id":userId, "merchant_id":merchantId] as [String : Any]
-        CouponNetwork.showProgress()
-        Alamofire.request(fullUrl, method:.post, parameters: parameter)
-            .validate().responseJSON(completionHandler: { (response) -> Void in
-                CouponNetwork.closeProgress()
-                if CouponNetwork.checkResponseData(response) {
-                    do {
-                        let checkCouponData:ExistenceCoupon = try JSONDecoder().decode(ExistenceCoupon.self, from: response.data!)
-                        complete(checkCouponData.isCouponData)
-                    } catch {
-                        complete(false)
-                    }
-                } else {
-                    complete(false)
-                }
-            })
-    }
-    
-    func deleteUserCoupon(userId:Int, merchantId:Int, complete: @escaping DataCallback) {
-        let fullUrl = "\(mainUrl)coupon_data"
-        let parameter = ["mode":"DeleteCouponData","user_id":userId, "merchant_id":merchantId] as [String : Any]
-        CouponNetwork.showProgress()
-        Alamofire.request(fullUrl, method:.post, parameters: parameter)
-            .validate().responseJSON(completionHandler: { (response) -> Void in
-                CouponNetwork.closeProgress()
-                if CouponNetwork.checkResponseData(response) {
-                    complete(true)
-                } else {
-                    complete(false)
-                }
-            })
-    }
-    
-    func getUserCouponData(userId:Int, complete: @escaping (Bool, UserCouponList?) -> Void) {
-        let fullUrl = "\(mainUrl)coupon_data"
-        let parameter = ["mode":"GetCouponData","user_id":userId] as [String : Any]
-        Alamofire.request(fullUrl, method:.post, parameters: parameter)
-            .validate().responseJSON(completionHandler: { (response) -> Void in
-                if CouponNetwork.checkResponseData(response) {
-                    do {
-                        let userCouponList:UserCouponList = try JSONDecoder().decode(UserCouponList.self, from: response.data!)
-                        complete(true,userCouponList)
-                    } catch {
-                        complete(false,nil)
-                    }
-                } else {
-                    complete(false,nil)
-                }
-            })
-    }
-    
-    func updateUesrCoupon(userId:Int, merchantId:Int, couponCount:Int, complete: @escaping DataCallback) {
-        let fullUrl = "\(mainUrl)coupon_data"
-        let parameter = ["mode":"UpdateCouponData","user_id":userId, "merchant_id":merchantId, "coupon_count":couponCount] as [String : Any]
-        CouponNetwork.showProgress()
-        Alamofire.request(fullUrl, method:.post, parameters: parameter)
-            .validate().responseJSON(completionHandler: { (response) -> Void in
-                CouponNetwork.closeProgress()
-                if CouponNetwork.checkResponseData(response) {
-                    complete(true)
-                } else {
-                    complete(false)
-                }
-            })
-    }
-    
-    // MARK: - 기타등등
-    static func checkResponseData(_ response:DataResponse<Any>) -> Bool {
-        switch response.result {
-        case .success(_):
-            do {
-                let respCode:ResponseCode = try JSONDecoder().decode(ResponseCode.self, from: response.data!)
-                return respCode.isSuccess
-            } catch {
-                print("error = \(error.localizedDescription)")
-                print("response = \(response)")
-                return false
-            }
-        case .failure(let error):
-            print("error = \(error.localizedDescription)")
-            print("response = \(response)")
-            return false
+  }
+
+  func getUserData(phoneNumber:String, complete: @escaping DataCallback) {
+    let fullUrl = "\(mainUrl)user_data"
+    let parameter = ["phone_number":phoneNumber, "mode":"GetUserInfo"]
+    CouponNetwork.showProgress()
+    let request = AF.request(URL(fileURLWithPath: fullUrl), method: .post, parameters: parameter)
+    request.responseJSON { (response) -> Void in
+      CouponNetwork.closeProgress()
+      if CouponNetwork.checkResponseData(response) {
+        do {
+          CouponSignleton.instance.userData =  try JSONDecoder().decode(User.self, from: response.data!)
+          complete(true)
+        } catch {
+          complete(false)
         }
+      } else {
+        complete(false)
+      }
     }
-    
-    static func showProgress() {
-        UIApplication.shared.beginIgnoringInteractionEvents()
-        SVProgressHUD.show()
+  }
+
+  func checkPassword(phoneNumber:String, password:String, complete: @escaping DataCallback) {
+    let fullUrl = "\(mainUrl)user_data"
+    let parameter = ["phone_number":phoneNumber, "password":password, "mode":"CheckUserPassword"]
+    CouponNetwork.showProgress()
+    let request = AF.request(URL(fileURLWithPath: fullUrl), method: .post, parameters: parameter)
+    request.responseJSON {  (response) -> Void in
+      CouponNetwork.closeProgress()
+      if CouponNetwork.checkResponseData(response) {
+        do {
+          CouponSignleton.instance.userData =  try JSONDecoder().decode(User.self, from: response.data!)
+          complete(true)
+        } catch {
+          complete(false)
+        }
+      } else {
+        complete(false)
+      }
     }
-    
-    static func closeProgress() {
-        UIApplication.shared.endIgnoringInteractionEvents()
-        SVProgressHUD.dismiss()
+  }
+
+  func getMerchantData(complete: @escaping DataCallback) {
+    let fullUrl = "\(mainUrl)merchant_data"
+    let parameter = ["mode":"GetMerchantData"]
+    CouponNetwork.showProgress()
+    let request = AF.request(URL(fileURLWithPath: fullUrl), method: .post, parameters: parameter)
+    request.responseJSON { (response) -> Void in
+      CouponNetwork.closeProgress()
+      if CouponNetwork.checkResponseData(response) {
+        do {
+          CouponSignleton.instance.merchantList = try JSONDecoder().decode(MerchantImplList.self, from: response.data!)
+          complete(true)
+        } catch {
+          print("error \(error)")
+          complete(false)
+        }
+      } else {
+        complete(false)
+      }
     }
+  }
+
+  func insertUserCoupon(userId:Int, merchantId:Int, complete: @escaping DataCallback) {
+    let fullUrl = "\(mainUrl)coupon_data"
+    let parameter = ["mode":"InsertCouponData","user_id":userId, "merchant_id":merchantId] as [String : Any]
+    CouponNetwork.showProgress()
+    let request = AF.request(URL(fileURLWithPath: fullUrl), method: .post, parameters: parameter)
+    request.responseJSON { (response) -> Void in
+      CouponNetwork.closeProgress()
+      if CouponNetwork.checkResponseData(response) {
+        complete(true)
+      } else {
+        complete(false)
+      }
+    }
+  }
+
+  func checkUserCoupon(userId:Int, merchantId:Int, complete: @escaping DataCallback) {
+    let fullUrl = "\(mainUrl)coupon_data"
+    let parameter = ["mode":"CheckCouponData","user_id":userId, "merchant_id":merchantId] as [String : Any]
+    CouponNetwork.showProgress()
+    let request = AF.request(URL(fileURLWithPath: fullUrl), method: .post, parameters: parameter)
+    request.responseJSON { (response) -> Void in
+      CouponNetwork.closeProgress()
+      if CouponNetwork.checkResponseData(response) {
+        do {
+          let checkCouponData:ExistenceCoupon = try JSONDecoder().decode(ExistenceCoupon.self, from: response.data!)
+          complete(checkCouponData.isCouponData)
+        } catch {
+          complete(false)
+        }
+      } else {
+        complete(false)
+      }
+    }
+  }
+
+  func deleteUserCoupon(userId:Int, merchantId:Int, complete: @escaping DataCallback) {
+    let fullUrl = "\(mainUrl)coupon_data"
+    let parameter = ["mode":"DeleteCouponData","user_id":userId, "merchant_id":merchantId] as [String : Any]
+    CouponNetwork.showProgress()
+
+    let request = AF.request(URL(fileURLWithPath: fullUrl), method: .post, parameters: parameter)
+    request.responseJSON { (response) -> Void in
+      CouponNetwork.closeProgress()
+      if CouponNetwork.checkResponseData(response) {
+        complete(true)
+      } else {
+        complete(false)
+      }
+    }
+  }
+
+  func getUserCouponData(userId:Int, complete: @escaping (Bool, UserCouponList?) -> Void) {
+    let fullUrl = "\(mainUrl)coupon_data"
+    let parameter = ["mode":"GetCouponData","user_id":userId] as [String : Any]
+    let request = AF.request(URL(fileURLWithPath: fullUrl), method: .post, parameters: parameter)
+    request.responseJSON { (response) -> Void in
+      if CouponNetwork.checkResponseData(response) {
+        do {
+          let userCouponList:UserCouponList = try JSONDecoder().decode(UserCouponList.self, from: response.data!)
+          complete(true,userCouponList)
+        } catch {
+          complete(false,nil)
+        }
+      } else {
+        complete(false,nil)
+      }
+    }
+  }
+
+  func updateUesrCoupon(userId:Int, merchantId:Int, couponCount:Int, complete: @escaping DataCallback) {
+    let fullUrl = "\(mainUrl)coupon_data"
+    let parameter = ["mode":"UpdateCouponData","user_id":userId, "merchant_id":merchantId, "coupon_count":couponCount] as [String : Any]
+    CouponNetwork.showProgress()
+    let request = AF.request(URL(fileURLWithPath: fullUrl), method: .post, parameters: parameter)
+    request.responseJSON{ (response) -> Void in
+      CouponNetwork.closeProgress()
+      if CouponNetwork.checkResponseData(response) {
+        complete(true)
+      } else {
+        complete(false)
+      }
+    }
+  }
+
+  // MARK: - 기타등등
+  static func checkResponseData(_ response: AFDataResponse<Any>) -> Bool {
+    switch response.result {
+    case .success(_):
+      do {
+        let respCode:ResponseCode = try JSONDecoder().decode(ResponseCode.self, from: response.data!)
+        return respCode.isSuccess
+      } catch {
+        print("error = \(error.localizedDescription)")
+        print("response = \(response)")
+        return false
+      }
+    case .failure(let error):
+      print("error = \(error.localizedDescription)")
+      print("response = \(response)")
+      return false
+    }
+  }
+
+  static func showProgress() {
+    UIApplication.shared.beginIgnoringInteractionEvents()
+    SVProgressHUD.show()
+  }
+
+  static func closeProgress() {
+    UIApplication.shared.endIgnoringInteractionEvents()
+    SVProgressHUD.dismiss()
+  }
 }
