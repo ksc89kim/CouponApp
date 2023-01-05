28f21ed1c15174a9ae3c2b8147c9d284f35266ae
seonchul.kim <ksc89kim@naver.com>
2023-01-06 08:16:30 +0900

    Me User데이터 -> RxRelay로 변경

--- CouponApp/CouponApp/Module/User/Me.swift
+++ CouponApp/CouponApp/Module/User/Me.swift
@@ -8,25 +8,19 @@
 
 import Foundation
 import RxSwift
+import RxRelay
 
 final class Me {
-
-  // MARK: - Define
-
-  enum Constant {
-    static let updateMeNotification: Notification.Name = .init("UpdateMeNotification")
-  }
 
   // MARK: - Property
 
   static var instance: Me = .init()
-  private var user: User?
+  fileprivate let userReleay: BehaviorRelay<User?> = .init(value: nil)
 
   // MARK: - Method
 
   func update(user: User) {
-    self.user = user
-    NotificationCenter.default.post(.init(name: Constant.updateMeNotification, object: user))
+    self.userReleay.accept(user)
   }
 }
 
@@ -37,11 +31,8 @@ extension Me: ReactiveCompatible {
 
 extension Reactive where Base: Me {
   var user: Observable<User> {
-    return NotificationCenter.default.rx
-      .notification(Me.Constant.updateMeNotification)
-      .compactMap { (notification: Notification) -> User? in
-        return notification.object as? User
-      }
+    return self.base.userReleay
+      .filterNil()
       .filter { (user: User) in user.isVaildID }
   }
 
